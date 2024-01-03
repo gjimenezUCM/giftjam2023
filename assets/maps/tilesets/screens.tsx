19c6c841f9f55429b4502456abf8e9be38b9fb6f
gjimenezUCM <gjimenez@ucm.es>
2024-01-03 15:35:18 +0000

    Deploying to gh-pages from @ gjimenezUCM/giftjam2023@13066b5d5d2ad0477b6e7f01922070226bb43a64 🚀

---
+++ assets/maps/tilesets/screens.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="screens" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="../../sprites/room/screens.png" width="512" height="448"/>
+</tileset>
---
+++ assets/maps/tilesets/wall-floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="wall-floor" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="../../sprites/room/wall-floor.png" width="512" height="448"/>
+</tileset>
